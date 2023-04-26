46009254445450b4f0872df5ed9dc8a1bb1f4b26
hueywen <hueywen8018@gmail.com>
2023-04-26 23:06:56 +0800

    added footer and header with react-bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
+import { Container } from 'react-bootstrap';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <h1>Welcome to Proshop</h1>
+    <>
+    <Header />
+      <main>
+      <Container>
+            <h1>Welcome to Proshop</h1>
+            
+
+          </Container>
+      </main>
+    <Footer />
+
+    </>
+    
   );
 }
 
