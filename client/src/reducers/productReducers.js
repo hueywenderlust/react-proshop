d996c0b4c9ba9ebc74cc784474119233ba29ceb6
hueywen <hueywen8018@gmail.com>
2023-04-30 17:40:07 +0800

    added product details reducer

--- client/src/reducers/productReducers.js
+++ client/src/reducers/productReducers.js
@@ -3,8 +3,12 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
+// product list 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
@@ -17,6 +21,26 @@ export const productListReducer = (state = { products: [] }, action) => {
       }
 
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+
+    default:
+      return state
+  }
+}
+
+// product details
+export const productDeatilsReducer = (state = { product: { reviews: [] } }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
+
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload,
+      }
+
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
 
     default:
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,29 +1,28 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useParams } from 'react-router-dom';
-import axios from 'axios'
+import { listProductDetails } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 
 const ProductScreen = () => {
 
     const { id } = useParams();
-
-    const [product, setProduct] = useState({}); 
+    const dispatch = useDispatch()
+    const productDetails = useSelector((state) => state.productDetails)
+    const { loading, error, product } = productDetails
 
     useEffect(() => {
-        const fetchProduct = async () => {
-          const { data } = await axios.get(`/api/products/${id}`);
-    
-          setProduct(data)
-        } 
-    
-        fetchProduct()
-      }, [id]);
 
+      dispatch(listProductDetails(id))
 
-    // const product = products.find((p) => String(p._id) === id);
+     
+
+      }, [dispatch, id]);
 
 
   return (
@@ -32,57 +31,64 @@ const ProductScreen = () => {
         Go Back
     </Link>
 
-    <Row>
-        <Col md={7} >
-            <Image src={product.image} alt={product.name} fluid/>
-        </Col>
-        <Col md={5} >
-            <ListGroup variant='flush'>
-                <ListGroupItem>
-                    <h4>{product.name}</h4>
-                </ListGroupItem>
+     {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
 
-                <ListGroup.Item>
-                    <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-                </ListGroup.Item>
-
-                <ListGroup.Item>
-                    Description: {product.description} 
-                </ListGroup.Item>
-
-                <ListGroup.Item>
-                    <h2 className='productPrice'>${product.price}</h2> 
-
+        <Row>
+            <Col md={7} >
+                <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+            <Col md={5} >
+                <ListGroup variant='flush'>
                     <ListGroupItem>
-                        <Row>
-                            {/* <Col>Status:</Col> */}
-                            <Col><strong>{product.countInStock > 0 ? 'In stock': 'Out of stock'}</strong></Col>
-                        </Row>
+                        <h4>{product.name}</h4>
                     </ListGroupItem>
 
-                    <Row className='buttonContainer'>
-                        <Col>
-                            <Button className='btn btn-block btn-info' type='button'>
-                                Add to wishlist
-                            </Button>
-                        </Col>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+                    </ListGroup.Item>
 
-                        <Col>
-                            <Button className='btn btn-block btn-success' type='button' disabled={product.countInStock === 0 }>
-                                Add to Cart
-                            </Button>
-                        </Col>
+                    <ListGroup.Item>
+                        Description: {product.description} 
+                    </ListGroup.Item>
 
-                        
-                    </Row>
-                  
-                   
-                </ListGroup.Item>
-            </ListGroup>
-        </Col>
-        
+                    <ListGroup.Item>
+                        <h2 className='productPrice'>${product.price}</h2> 
 
-    </Row>
+                        <ListGroupItem>
+                            <Row>
+                                {/* <Col>Status:</Col> */}
+                                <Col><strong>{product.countInStock > 0 ? 'In stock': 'Out of stock'}</strong></Col>
+                            </Row>
+                        </ListGroupItem>
+
+                        <Row className='buttonContainer'>
+                            <Col>
+                                <Button className='btn btn-block btn-info' type='button'>
+                                    Add to wishlist
+                                </Button>
+                            </Col>
+
+                            <Col>
+                                <Button className='btn btn-block btn-success' type='button' disabled={product.countInStock === 0 }>
+                                    Add to Cart
+                                </Button>
+                            </Col>
+
+                            
+                        </Row>
+                    
+                    
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            
+
+        </Row>
+      )}
     </>
   )
 }
