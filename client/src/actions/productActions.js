19539cc3cb55b684c8ac32a77e23b2b2f3942cab
hueywen <hueywen8018@gmail.com>
2023-04-30 17:03:19 +0800

    added loader, message components

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,25 +1,28 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productConstants'
+import {
+    PRODUCT_LIST_REQUEST, 
+    PRODUCT_LIST_SUCCESS, 
+    PRODUCT_LIST_FAIL
+} from '../constants/productConstants'
 import axios from 'axios'
+
 // function within function with thunk
-export const listProducts = () => async(dispatch) => {
-    try {
-        dispatch({ 
-            type: PRODUCT_LIST_REQUEST 
-        })
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST })
 
-        const { data } = await axios.get('api/products')
+    const { data } = await axios.get('/api/products')
 
-        dispatch({ 
-            type: PRODUCT_LIST_SUCCESS, 
-            payload: data 
-        })
-
-    } catch (error) {
-        
-        dispatch({ 
-            type: PRODUCT_LIST_FAIL, 
-            payload:  error.response && error.response.data.message ? error.response.data.message : error.message, 
-        })
-
-    }
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
 }
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
-import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
+import {Row, Col, Image, ListGroup, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { useParams } from 'react-router-dom';
 import axios from 'axios'
